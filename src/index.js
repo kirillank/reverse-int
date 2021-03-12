93f191e4a350839fcd87db113a3a557a0c9b2090
kirillank <kirill.ank04@yandex.by>
2021-03-12 18:31:36 +0300

    fix:fix code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  var num = 0;
+  if (n>0){
+  num = n.toString().split('').reverse().join('');
+  }else{
+      n=n*-1;
+      num = n.toString().split('').reverse().join('');
+  }
+  return num
 }
